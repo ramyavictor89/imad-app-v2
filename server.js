fb14e22c2b5ff56c7a62b4d7a07d244c2a92c9e0
ramyavictor89 <rudiprocks@yaoo.co.in>
2017-02-03 17:01:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,17 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function (req, res) {
+  res.send('Article one will be loaded');
+});
+
+app.get('/article-two', function (req, res) {
+  res.send('Article two will be loaded');
+});
+
+app.get('/article-three', function (req, res) {
+  res.send('article three will be loaded');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
