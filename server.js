509f203a19e3d0d78ef288a46d6e2f80e8e5cd5d
ramyavictor89 <rudiprocks@yaoo.co.in>
2017-02-05 13:34:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -86,6 +86,11 @@ function createTemplate(data){
     return htmlTemplate;
 
 }
+var counter = 0;
+app.get('/counter',function(req,res){
+	counter = counter + 1;
+	res.send(counter.toString());
+});
 
 app.get('/:articleName', function (req, res) {
   var articleName = req.params.articleName;
@@ -98,6 +103,10 @@ app.get('/', function (req, res) {
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/main.js',function (req,res){
+	res.sendFile(path.join(__dirname,'ui','main.js'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
