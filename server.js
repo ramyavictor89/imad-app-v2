bc51e85e2c32cbfdb15c84a7723168e4b553bb89
ramyavictor89 <rudiprocks@yaoo.co.in>
2017-03-26 20:05:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -107,7 +107,7 @@ app.get('/articles/:articleName',function(req,res){
             if(result.rows.length == 0){
                 result.status(404).send('Article not found');
             }else{
-                var articleData = results.row[0];
+                var articleData = results.rows[0];
                 res.send(createTemplate(articleData));
             }
         }
