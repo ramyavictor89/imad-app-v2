4f97c63824885df189c0bd096757ed14bbe7ac62
ramyavictor89 <rudiprocks@yaoo.co.in>
2017-03-26 19:57:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,9 +2,18 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var crypto = require('crypto');
+var Pool = require('pg').Pool;
 
 var app = express();
 app.use(morgan('combined'));
+
+var config = {
+  host: 'db.imad.hasura-app.io',
+  user: 'ramyavictor89',
+  password: process.env.DB_PASSWORD,
+  database: 'ramyavictor89',
+};
+
 
 var articles = {
     'article-one' : {
@@ -88,6 +97,24 @@ function createTemplate(data){
 
 }
 var counter = 0;
+
+var pool = new Pool(config);
+app.get('/articles/:articleName',function(req,res){
+    pool.query("SELECT * FROM article WHERE title = $1",[req.params.articleName], function(err,result){
+        
+        if(err){
+            res.status(500).send(err.toString());
+        }else{
+            if(result.rows.legth == 0){
+                result.status(404).send('Article not found');
+            }else{
+                var articleData = results.row[0];
+                res.send(createTemplate(articleData));
+            }
+        }
+    })
+})
+
 app.get('/counter',function(req,res){
 	counter = counter + 1;
 	res.send(counter.toString());
